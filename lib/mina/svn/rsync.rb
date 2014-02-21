e65ce7854e31afb5daafe8e4afc5dca8909b574d
robbinhan <luckyhanbin@gmail.com>
2014-02-21 18:16:17 +0800

    first commit

---
+++ lib/mina/svn/rsync.rb
@@ -0,0 +1,9 @@
+require "mina/svn/rsync/version"
+
+module Mina
+  module Svn
+    module Rsync
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/mina/svn/rsync/version.rb
@@ -0,0 +1,7 @@
+module Mina
+  module Svn
+    module Rsync
+      VERSION = "0.0.1"
+    end
+  end
+end
